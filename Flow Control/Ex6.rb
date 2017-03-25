a8d6d9eba48a4df356d418996919b05e135e5f00
Gooi <gooiyingchyi@gmail.com>
2017-03-25 22:10:27 +0000

    third commit

--- Flow Control/Ex6.rb
+++ Flow Control/Ex6.rb
@@ -5,6 +5,7 @@ def equal_to_four(x)
     puts "yup"
   else
     puts "nope"
+  #we are missing an 'end' keyword here. Since the 'end' here gets matched with the if...else statement, ruby expects the keyword 'end' to close off our method.
 end
 
 equal_to_four(5)
